3aba662a7f0a697624e2c465b63bbb2074f65bf7
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-08-09 16:36:02 +0200

    Defines configuration module and ability to provide cartesian product of selected verticals

---
+++ core/configuration/configuration.go
@@ -0,0 +1,57 @@
+package configuration
+
+import (
+    "gopkg.in/yaml.v2"
+    "github.com/ecooper/combinatoric"
+)
+
+type TestRunnerConfiguration struct {
+    Description, Path string
+    BuildToolConfiguration `yaml:"build_tool,flow"`
+    EnvironmentVerticals `yaml:"verticals,inline"`
+}
+
+type BuildToolConfiguration struct {
+    Command string `yaml:"cmd"`
+}
+
+type EnvironmentVerticals struct {
+    Verticals map[string][]string `yaml:"verticals,flow"`
+}
+
+func Product(v EnvironmentVerticals) [][]string {
+    values := make([][]interface{}, 0, len(v.Verticals))
+    for _, value := range v.Verticals {
+        values = append(values, sliceOfInterfaces(value))
+    }
+    pIter, _ := combinatoric.Product(values)
+    products := make([][]string, 0, len(values))
+    for pIter.HasNext() {
+        products = append(products, sliceOfStrings(pIter.Next()))
+    }
+    return products
+}
+
+func Read(configuration string) (TestRunnerConfiguration, error) {
+    var c TestRunnerConfiguration = TestRunnerConfiguration{}
+    if err := yaml.Unmarshal([]byte(configuration), &c); err != nil {
+        return c, err
+    }
+    return c, nil
+}
+
+func sliceOfInterfaces(s []string) []interface{} {
+    result := make([]interface{}, len(s))
+    for i, v := range s {
+        result[i] = v
+    }
+    return result
+}
+
+func sliceOfStrings(s []interface{}) []string {
+    result := make([]string, len(s))
+    for i, v := range s {
+        result[i] = v.(string)
+    }
+    return result
+}
---
+++ core/configuration/configuration_test.go
@@ -0,0 +1,122 @@
+package configuration_test
+
+import (
+    cfg "github.com/almighty/almighty-test-runner/core/configuration"
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+)
+
+
+var _ = Describe("Test runner configuration", func() {
+
+    Context("Loading build settings", func() {
+
+        It("Should load build command", func() {
+            // given
+            configurationAsYAML := `
+            build_tool:
+                cmd: mvn clean install -P all
+            `
+
+            // when
+            configuration, _ := cfg.Read(configurationAsYAML)
+
+            // then
+            Expect(configuration.Command).To(Equal("mvn clean install -P all"))
+        })
+
+        It("Should load description", func() {
+            // given
+            configurationAsYAML := `
+            description: "I'm GROOT!"
+            build_tool:
+                cmd: mvn clean install -P all
+            `
+
+            // when
+            configuration, _ := cfg.Read(configurationAsYAML)
+
+            // then
+            Expect(configuration.Description).To(Equal("I'm GROOT!"))
+        })
+
+        It("Should load path", func() {
+            // given
+            configurationAsYAML := `
+            path: /tmp/project/
+            `
+
+            // when
+            configuration, _ := cfg.Read(configurationAsYAML)
+
+            // then
+            Expect(configuration.Path).To(Equal("/tmp/project/"))
+        })
+
+    })
+
+    Context("Enviroment verticals", func() {
+        It("Should load verticals", func() {
+            // given
+            configurationAsYAML := `
+            build_tool:
+                cmd: mvn clean test
+            verticals:
+                database: [hsqldb, oracle11g, mysql, postgresql, mssql]
+                runtime: [dropwizard, wildfly-swarm, express.js, goa]
+            `
+
+            // when
+            configuration, _ := cfg.Read(configurationAsYAML)
+
+            // when
+            Expect(configuration.Verticals).To(HaveKeyWithValue("database", []string {"hsqldb", "oracle11g", "mysql", "postgresql", "mssql"}))
+            Expect(configuration.Verticals).To(HaveKeyWithValue("runtime", []string {"dropwizard", "wildfly-swarm", "express.js", "goa"}))
+        })
+
+        It("Should provide cartesian product of all verticals", func() {
+            // given
+            configurationAsYAML := `
+            verticals:
+                database: [hsqldb, oracle11g, mysql, postgresql, mssql]
+                runtime: [dropwizard, wildfly-swarm, express.js, goa]
+            `
+            configuration, _ := cfg.Read(configurationAsYAML)
+
+            // when
+            products := cfg.Product(configuration.EnvironmentVerticals)
+
+            // when
+            Expect(products).To(HaveLen(len(allProducts())))
+            Expect(products).To(ConsistOf(allProducts()))
+        })
+    })
+
+})
+
+// -- Test fixtures
+func allProducts() [][]string {
+    return [][]string{
+        {"hsqldb", "dropwizard"},
+        {"hsqldb", "wildfly-swarm"},
+        {"hsqldb", "express.js"},
+        {"hsqldb", "goa"},
+        {"oracle11g", "dropwizard"},
+        {"oracle11g", "wildfly-swarm"},
+        {"oracle11g", "express.js"},
+        {"oracle11g", "goa"},
+        {"mysql", "dropwizard"},
+        {"mysql", "wildfly-swarm"},
+        {"mysql", "express.js"},
+        {"mysql", "goa"},
+        {"postgresql", "dropwizard"},
+        {"postgresql", "wildfly-swarm"},
+        {"postgresql", "express.js"},
+        {"postgresql", "goa"},
+        {"mssql", "dropwizard"},
+        {"mssql", "wildfly-swarm"},
+        {"mssql", "express.js"},
+        {"mssql", "goa"},
+
+    }
+}
