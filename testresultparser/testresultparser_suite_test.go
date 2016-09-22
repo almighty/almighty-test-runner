5307643889e3fdd9bb882b600c3193ec150ec5a1
Hemani <hemani@dhcp35-120.lab.eng.blr.redhat.com>
2016-09-22 18:41:21 +0530

    updated tests to ginkgo

---
+++ testresultparser/testresultparser_suite_test.go
@@ -0,0 +1,13 @@
+package testresultparser_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestTestresultparser(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Testresultparser Suite")
+}
